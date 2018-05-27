291ad0a547227fb8975f3af0060317755357160a
Soenke Ruempler <soenke@ruempler.eu>
2018-05-27 18:46:47 +0200

    initial skeleton

---
+++ README.md
@@ -0,0 +1,11 @@
+## What to explain
+
+Was beim Erstellen des Projektes alles aufgefallen ist, was erklaerenswer waere:
+
+ - IAM Role / Role Assume
+ - Lambda / ZIP (ggf. mal haendisch erstellen?)
+ - CloudFormation, welches erstellt wird
+ - oh nein, es packetiert ja viel zu viel!
+ - mein lokales python ist aber ein anderes
+ - regionen
+ 
---
+++ handler.py
@@ -0,0 +1,27 @@
+import json
+
+def get_url(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+def create_url(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
