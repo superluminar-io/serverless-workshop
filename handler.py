adaa0885de6762ba41ff753cda1c30b4be400481
Soenke Ruempler <soenke@ruempler.eu>
2018-05-29 19:43:32 +0200

    create url

--- handler.py
+++ handler.py
@@ -1,4 +1,7 @@
 import json
+from fnvhash import fnv1a_64
+import boto3
+import os
 
 def get_url(event, context):
     body = {
@@ -14,14 +17,20 @@ def get_url(event, context):
     return response
 
 def create_url(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    post_parameters = json.loads(event["body"])
+    url_to_shorten = post_parameters['url']
+    shortened_url = "{:x}".format(fnv1a_64(bytes(url_to_shorten, 'utf-8')))
 
-    return response
+    dynamodb = boto3.client('dynamodb')
+    dynamodb.put_item(
+        TableName=os.environ["DYNAMO_DB_TABLE"],
+        Item={
+            "short_url": {"S": shortened_url},
+            "url":       {"S": url_to_shorten},
+        }
+    )
+
+    return {
+        "statusCode": 201
+    }
