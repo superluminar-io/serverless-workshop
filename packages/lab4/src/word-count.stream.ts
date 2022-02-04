17fd0579ca4265001b6db68adfd6d7410f494247
Henrik Fricke <henrikfricke@web.de>
2022-02-04 14:13:31 +0100

    refactor(messaging): update documentation

--- packages/lab4/src/word-count.stream.ts
+++ packages/lab4/src/word-count.stream.ts
@@ -2,16 +2,16 @@ import * as AWS from 'aws-sdk';
 
 export const handler = async (event: AWSLambda.DynamoDBStreamEvent) => {
   const DB = new AWS.DynamoDB.DocumentClient();
-  const tableName = process.env.TABLE_NAME!;
 
   for (const record of event.Records) {
-    if (record.eventName !== 'INSERT' || !record.dynamodb) {
+    if (record.eventName !== 'INSERT' || !record.dynamodb || !record.dynamodb.NewImage) {
       return;
     }
 
-    const id = record.dynamodb.Keys!.id.S!;
-    const content = record.dynamodb.NewImage!.content.S!;
-    const wordCount = content.split(' ').length;
+    const id = record.dynamodb.Keys?.id.S;
+    const newImage = record.dynamodb.NewImage;
+    const content = newImage.content?.S;
+    const wordCount = content?.split(' ').length;
 
     await DB.update({
       Key: {
@@ -22,7 +22,7 @@ export const handler = async (event: AWSLambda.DynamoDBStreamEvent) => {
           Value: wordCount,
         },
       },
-      TableName: tableName,
+      TableName: process.env.TABLE_NAME!,
     }).promise();
   }
 };
