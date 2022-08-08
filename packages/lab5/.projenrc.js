a3ae1f63bdbd0448ccfb7040d28e7647132d045c
Robert von Massow <robert.von.massow@superluminar.io>
2022-08-08 16:56:43 +0200

    update lab 5

--- packages/lab5/.projenrc.js
+++ packages/lab5/.projenrc.js
@@ -1,18 +1,19 @@
 const { awscdk, javascript } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.35.0',
   defaultReleaseBranch: 'main',
   name: 'lab5',
   github: false,
   packageManager: javascript.NodePackageManager.NPM,
   deps: [
-    'aws-sdk',
+    '@aws-sdk/client-dynamodb',
+    '@aws-sdk/lib-dynamodb',
     'node-fetch@2',
     'fs-extra',
   ],
   devDeps: [
     '@types/aws-lambda',
-    'aws-sdk-mock',
+    'aws-sdk-client-mock',
     '@types/node-fetch@2',
     '@types/fs-extra',
   ],
@@ -21,5 +22,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 project.addTask('test:e2e', {
   exec: 'jest --testMatch "**/*.e2etest.ts"',
 });
-
+// Windows users might need this
+project.jest.addTestMatch('**/?(*.)+(spec|test).ts?(x)');
+project.jest.addIgnorePattern('.*/frontend/.*\\\.test\\\.[tj]sx?');
 project.synth();
